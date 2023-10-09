5e975a3ffd0b61b21cdfb86f4f15c13bd42313a4
ayaka <a.takahashi.sys23@morijyobi.ac.jp>
2023-10-09 10:47:27 +0900

    pra2.javaに出力処理追記

---
+++ pra2.java
@@ -0,0 +1,8 @@
+ public class pra2 {
+        public static void main(String[] args) {
+                for (int i = 0; i < 101; i++) {
+                for (int i = 1; i < 101; i++) {
+        System.out.println"(Hello World)";}
+        }
+ }
+
